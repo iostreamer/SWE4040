0704d3bf55815357ed910bf464cc35e23b5224e8
iostreamer <philippe.l.leger@gmail.com>
2014-01-09 16:48:48 -0400

    Serial Com test program
    The program will allow for two way communication with another MCU
    through the use of the Serial1 port. Arduino must be connected to PC in
    order to send the messages. The MCUs can either be connected directly to
    each other by wires or through the use of the bluetooth board and
    bluetooth modules. The modules need to be paired beforehand via PC.
    Currently, communication via bluetooth is possible, but there is an
    issue where the first part of a message gets jumbled and makes no sense
    which happens every time. Need to investigate further.

--- Arduino/sketch_jan09b_Serial_Test/sketch_jan09b_Serial_Test.ino
+++ Arduino/sketch_jan09b_Serial_Test/sketch_jan09b_Serial_Test.ino
@@ -1,19 +1,40 @@
 int incomingByte = 0;   // for incoming serial data
-
 void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  Serial1.begin(9600);
+  
+
 }
 
 void loop() {
+  
+    if (Serial.available() > 0) {
+      
 
-  // send data only when you receive data:
-  if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
 
     // say what you got:
-    Serial.print("I received: ");
-    Serial.println((char)incomingByte);
+    Serial1.print((char)incomingByte);
+    Serial.print((char)incomingByte);
+    
+
   }
+
+  // send data only when you receive data:
+  if (Serial1.available() > 0) {
+      
+
+    // read the incoming byte:
+    incomingByte = Serial1.read();
+
+    // say what you got:
+    Serial.print((char)incomingByte);
+    
+
+  }
+  
+  
+  
 }
 

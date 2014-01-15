8d1c597426d7acb3ab122843d47416756fe929c3
iostreamer <philippe.l.leger@gmail.com>
2014-01-15 17:44:09 -0400

    Corrected output format

--- Arduino/sketch_jan09b_Serial_Test/sketch_jan09b_Serial_Test.ino
+++ Arduino/sketch_jan09b_Serial_Test/sketch_jan09b_Serial_Test.ino
@@ -1,4 +1,4 @@
-char incomingByte = 0;   // for incoming serial data
+int incomingByte = 0;   // for incoming serial data
 void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
   Serial1.begin(9600);
@@ -7,29 +7,31 @@ void setup() {
 }
 
 void loop() {
-  
+    //Send
     if (Serial.available() > 0) {
       
 
+       
+      
     // read the incoming byte:
     incomingByte = Serial.read();
 
     // say what you got:
-    Serial1.println(incomingByte);
-    Serial.println(incomingByte);
+    Serial1.print((char)incomingByte);
+    Serial.print((char)incomingByte);
     
 
   }
 
-  // send data only when you receive data:
+  // Receive 
   if (Serial1.available() > 0) {
-      
-
+    
     // read the incoming byte:
     incomingByte = Serial1.read();
 
     // say what you got:
-    Serial.println(incomingByte);
+    //Serial.write(incomingByte);
+    Serial.print((char)incomingByte);
     
 
   }

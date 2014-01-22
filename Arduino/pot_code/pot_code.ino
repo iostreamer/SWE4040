685cc827d5247a0e50c72a2aa0380bbf4e14a8ef
iostreamer <philippe.l.leger@gmail.com>
2014-01-22 12:54:46 -0400

    Potentiometer code
    Created code to convert potentiometer reading to distance in order to
    simulate sensor input for distance.

---
+++ Arduino/pot_code/pot_code.ino
@@ -0,0 +1,18 @@
+  int sensorVal = 0;  
+
+  void setup() {
+ // Setup Serial communication with computer
+    Serial.begin(9600);
+  }
+
+  void loop() {
+ // Read the value from the sensor:
+    sensorVal = map(analogRead(A0),0,1023,0,30); //Maps pot input to distance measurment of 0 to 30 meters
+ 
+ // Send the value to the Serial Monitor
+    Serial.print("Sensor Value=");
+    Serial.println(sensorVal);
+
+ // Interval between readings = 1 second
+    delay(1000);                
+  }
